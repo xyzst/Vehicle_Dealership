e74407d248f130bdc2bfcc49ce89d868c5149719
Darren Rambaud <dtrambaud@txstate.edu>
2016-09-14 19:38:51 -0500

    test

--- src/dealership/Car.java
+++ src/dealership/Car.java
@@ -1,7 +1,5 @@
 package dealership;
 
-//import java.util.ArrayList;
-
 public class Car {
     private String VIN,
                    make,
--- src/dealership/database.java
+++ src/dealership/database.java
@@ -3,6 +3,7 @@ package dealership;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  *
@@ -13,13 +14,22 @@ public class database {
      * FIXME -- need description
      */
     public void importVehicleData() throws IOException {
+        Scanner fIn = null;
+
         try {
-            Scanner fIn = new Scanner(new FileReader("cars.txt"));
+            fIn = new Scanner(new FileReader("cars.txt"));
+            String line = fIn.nextLine();
+
+
         }
         catch (IOException ex){
             ex.printStackTrace();
-            System.out.println("\nERROR: \"cars.txt\" does NOT exist!" +
-                               "\nNow creating cars.txt ...");
+            System.out.println("\nERROR: \"cars.txt\" does NOT exist!");
+        }
+        finally{
+            if (fIn != null){
+                fIn.close();
+            }
         }
     }
 }
